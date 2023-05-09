d1708d292786687c758c59ac842bc3a00e6aa22a
brad-ellert <brad.ellert@cerebrahealth.com>
2023-05-09 16:48:01 -0600

    Rename from Birthday to Mother's Day

--- HappyMothersDay/app/src/main/java/com/example/happymothersday/MainActivity.kt
+++ HappyMothersDay/app/src/main/java/com/example/happymothersday/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.happybirthday
+package com.example.happymothersday
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
--- HappyMothersDay/app/src/test/java/com/example/happymothersday/ExampleUnitTest.kt
+++ HappyMothersDay/app/src/test/java/com/example/happymothersday/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.happybirthday
+package com.example.happymothersday
 
 import org.junit.Test
 
