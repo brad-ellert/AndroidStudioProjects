b28e97d3711f0b3a30458fc7fff9bc8c12c5b5da
brad-ellert <brad.ellert@cerebrahealth.com>
2023-05-12 11:14:30 -0600

    Refactor

--- android-basics-kotlin-lemonade-app-main/app/src/main/java/com/example/lemonade/MainActivity.kt
+++ android-basics-kotlin-lemonade-app-main/app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -24,26 +24,29 @@ import com.google.android.material.snackbar.Snackbar
 class MainActivity : AppCompatActivity() {
 
     /**
-     * DO NOT ALTER ANY VARIABLE OR VALUE NAMES OR THEIR INITIAL VALUES.
-     *
      * Anything labeled var instead of val is expected to be changed in the functions but DO NOT
      * alter their initial values declared here, this could cause the app to not function properly.
      */
-    private val LEMONADE_STATE = "LEMONADE_STATE"
-    private val LEMON_SIZE = "LEMON_SIZE"
-    private val SQUEEZE_COUNT = "SQUEEZE_COUNT"
-    // SELECT represents the "pick lemon" state
-    private val SELECT = "select"
-    // SQUEEZE represents the "squeeze lemon" state
-    private val SQUEEZE = "squeeze"
-    // DRINK represents the "drink lemonade" state
-    private val DRINK = "drink"
-    // RESTART represents the state where the lemonade has been drunk and the glass is empty
-    private val RESTART = "restart"
+    companion object {
+        private const val LEMONADE_STATE = "LEMONADE_STATE"
+        private const val LEMON_SIZE = "LEMON_SIZE"
+        private const val SQUEEZE_COUNT = "SQUEEZE_COUNT"
+    }
+
+    enum class LemonadeState {
+        SELECT,
+        SQUEEZE,
+        DRINK,
+        RESTART
+    }
+
+
     // Default the state to select
-    private var lemonadeState = "select"
+    private var lemonadeState = LemonadeState.SELECT
+
     // Default lemonSize to -1
     private var lemonSize = -1
+
     // Default the squeezeCount to -1
     private var squeezeCount = -1
 
@@ -55,31 +58,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // === DO NOT ALTER THE CODE IN THE FOLLOWING IF STATEMENT ===
         if (savedInstanceState != null) {
-            lemonadeState = savedInstanceState.getString(LEMONADE_STATE, "select")
+            lemonadeState =
+                savedInstanceState.getString(LEMONADE_STATE)?.let { LemonadeState.valueOf(it) }
+                    ?: LemonadeState.SELECT
             lemonSize = savedInstanceState.getInt(LEMON_SIZE, -1)
             squeezeCount = savedInstanceState.getInt(SQUEEZE_COUNT, -1)
         }
-        // === END IF STATEMENT ===
 
         lemonImage = findViewById(R.id.image_lemon_state)
         setViewElements()
-        lemonImage!!.setOnClickListener {
+        lemonImage?.setOnClickListener {
             clickLemonImage()
         }
-        lemonImage!!.setOnLongClickListener {
+        lemonImage?.setOnLongClickListener {
             showSnackbar()
         }
     }
 
     /**
-     * === DO NOT ALTER THIS METHOD ===
-     *
      * This method saves the state of the app if it is put in the background.
      */
     override fun onSaveInstanceState(outState: Bundle) {
-        outState.putString(LEMONADE_STATE, lemonadeState)
+        outState.putString(LEMONADE_STATE, lemonadeState.name)
         outState.putInt(LEMON_SIZE, lemonSize)
         outState.putInt(SQUEEZE_COUNT, squeezeCount)
         super.onSaveInstanceState(outState)
@@ -91,27 +92,27 @@ class MainActivity : AppCompatActivity() {
      */
     private fun clickLemonImage() {
         when (lemonadeState) {
-            SELECT -> {
+            LemonadeState.SELECT -> {
                 lemonSize = lemonTree.pick()
                 squeezeCount = 0
-                lemonadeState = SQUEEZE
+                lemonadeState = LemonadeState.SQUEEZE
             }
 
-            SQUEEZE -> {
+            LemonadeState.SQUEEZE -> {
                 squeezeCount++
                 lemonSize--
                 if (lemonSize == 0) {
-                    lemonadeState = DRINK
+                    lemonadeState = LemonadeState.DRINK
                     lemonSize = -1
                 }
             }
 
-            DRINK -> {
-                lemonadeState = RESTART
+            LemonadeState.DRINK -> {
+                lemonadeState = LemonadeState.RESTART
             }
 
-            RESTART -> {
-                lemonadeState = SELECT
+            LemonadeState.RESTART -> {
+                lemonadeState = LemonadeState.SELECT
             }
         }
         setViewElements()
@@ -122,42 +123,43 @@ class MainActivity : AppCompatActivity() {
      */
     private fun setViewElements() {
         val textAction: TextView = findViewById(R.id.text_action)
+        var text: Int = 0
+        var image: Int = 0
         when (lemonadeState) {
-            SELECT -> {
-                textAction.setText(R.string.lemon_select)
-                lemonImage!!.setImageResource(R.drawable.lemon_tree)
+            LemonadeState.SELECT -> {
+                text = R.string.lemon_select
+                image = R.drawable.lemon_tree
             }
 
-            SQUEEZE -> {
-                textAction.setText(R.string.lemon_squeeze)
-                lemonImage!!.setImageResource(R.drawable.lemon_squeeze)
+            LemonadeState.SQUEEZE -> {
+                text = R.string.lemon_squeeze
+                image = R.drawable.lemon_squeeze
             }
 
-            DRINK -> {
-                textAction.setText(R.string.lemon_drink)
-                lemonImage!!.setImageResource(R.drawable.lemon_drink)
+            LemonadeState.DRINK -> {
+                text = R.string.lemon_drink
+                image = R.drawable.lemon_drink
             }
 
-            RESTART -> {
-                textAction.setText(R.string.lemon_empty_glass)
-                lemonImage!!.setImageResource(R.drawable.lemon_restart)
+            LemonadeState.RESTART -> {
+                text = R.string.lemon_empty_glass
+                image = R.drawable.lemon_restart
             }
         }
+        textAction.setText(text)
+        lemonImage?.setImageResource(image)
     }
 
     /**
-     * === DO NOT ALTER THIS METHOD ===
-     *
      * Long clicking the lemon image will show how many times the lemon has been squeezed.
      */
     private fun showSnackbar(): Boolean {
-        if (lemonadeState != SQUEEZE) {
+        if (lemonadeState != LemonadeState.SQUEEZE) {
             return false
         }
-        val squeezeText = getString(R.string.squeeze_count, squeezeCount)
         Snackbar.make(
             findViewById(R.id.constraint_Layout),
-            squeezeText,
+            getString(R.string.squeeze_count, squeezeCount),
             Snackbar.LENGTH_SHORT
         ).show()
         return true
